ae6784f4b9f814dbe625c47505fa9c2bc32b32ee
Yidi Sprei <yidi.sprei@yidienterprises.com>
2024-08-26 14:35:37 -0400

    Refactor request body handling with io.NopCloser for better efficiency

--- infuzu/integrations/gin/middleware.go
+++ infuzu/integrations/gin/middleware.go
@@ -1,17 +1,18 @@
 package infuzu
 
 import (
+	"bytes"
 	"github.com/gin-gonic/gin"
 	authenticate "github.com/infuzu/infuzu-go-sdk/infuzu/authentication/authenticate"
 	shortcuts "github.com/infuzu/infuzu-go-sdk/infuzu/authentication/shortcuts"
-	"net/http"
+	"io"
 )
 
 func VerifyAndIdentifyMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		signature := c.GetHeader(shortcuts.SignatureHeaderName)
 		message, _ := c.GetRawData()
-		c.Request.Body = http.MaxBytesReader(c.Writer, c.Request.Body, int64(len(message)))
+		c.Request.Body = io.NopCloser(bytes.NewBuffer(message))
 		application, err := authenticate.ConvertMessageSignatureToApplicationAndVerify(signature, string(message))
 		if err != nil {
 			c.Set("application", nil)
